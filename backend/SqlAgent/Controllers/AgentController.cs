25a953d4e59e783f8f4ef80f1c8ce9aee748e760
biswarup-naha <biswarup.naha@capsitech.com>
2026-03-26 16:41:25 +0530

    kubernetes config added

--- backend/SqlAgent/Controllers/AgentController.cs
+++ backend/SqlAgent/Controllers/AgentController.cs
@@ -88,32 +88,32 @@ namespace SqlAgent.Controllers
             return res;
         }
 
-        [HttpPost]
-        //[Authorize(Roles = "USER, ADMIN")]
-        public async Task<ApiResponse<string>> AskAgent([FromBody] PromptRequest req)
-        {
-            string query = req.Prompt;
-            var res = new ApiResponse<string>();
-            try
-            {
-                //var ans = await _agent.QueryAgent(query);
-                //if (User.IsInRole("SUPERADMIN"))
-                //{
-                res.Result = await _agent.QueryAgent(query); ;
-                res.Message = "Query successfully processed";
-                res.Status = true;
-                //}
-                //res.Status = true;
-                //res.Message = "FAQ statistics retrieved successfully";
-                //res.Result = stats;
-            }
-            catch (Exception ex)
-            {
-                res.Status = false;
-                res.Message = ex.Message;
-            }
-            return res;
-        }
+        //[HttpPost]
+        ////[Authorize(Roles = "USER, ADMIN")]
+        //public async Task<ApiResponse<string>> AskAgent([FromBody] PromptRequest req)
+        //{
+        //    string query = req.Prompt;
+        //    var res = new ApiResponse<string>();
+        //    try
+        //    {
+        //        //var ans = await _agent.QueryAgent(query);
+        //        //if (User.IsInRole("SUPERADMIN"))
+        //        //{
+        //        res.Result = await _agent.QueryAgent(query); ;
+        //        res.Message = "Query successfully processed";
+        //        res.Status = true;
+        //        //}
+        //        //res.Status = true;
+        //        //res.Message = "FAQ statistics retrieved successfully";
+        //        //res.Result = stats;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        res.Status = false;
+        //        res.Message = ex.Message;
+        //    }
+        //    return res;
+        //}
     }
 
     public record PromptRequest(string Prompt);
